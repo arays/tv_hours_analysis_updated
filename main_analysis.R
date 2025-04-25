f4b1263b10b1fc55245861bbf525fef73f6efbff
arays <33163187+arays@users.noreply.github.com>
2025-04-25 19:34:29 +0300

    filter those  30 years and  older

--- main_analysis.R
+++ main_analysis.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(here)
 tv_hours_table <- gss_cat %>% 
-filter(age <30)%>%   
+filter(age >=30)%>%   
 group_by(marital) %>% 
   summarize(mean_tv_hours=mean(tvhours, na.rm=T))
 write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))  
